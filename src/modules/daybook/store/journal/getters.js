ea7189b1ca17a60534f8c00e10d0073fcf3a2a32
Facundo Tanovich <facundotanovich@gmail.com>
2022-08-04 16:51:24 -0300

    Primer commit

---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,7 @@
+export const getEntriesByTerm = (/*state*/) => {
+    
+}
+
+export const getEntriesById = (/*state*/) => {
+    
+}
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,25 @@
+
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Aliqua consectetur ea commodo quis velit incididunt. Nulla anim laborum sit occaecat. Cillum id ipsum nisi anim magna labore ipsum nostrud.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Eu tempor occaecat ut occaecat laborum do. Sint est labore laboris esse duis esse anim occaecat enim. Dolor esse nisi ut reprehenderit sit irure aliquip cillum culpa culpa nulla esse irure ex. Adipisicing eu laborum occaecat pariatur sint aliquip. Quis voluptate commodo deserunt cupidatat sunt dolore minim do. Fugiat nostrud magna ullamco pariatur pariatur qui nulla consequat. Commodo anim ea irure anim in laborum irure magna.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Aliqua magna pariatur laborum adipisicing nostrud. Qui nostrud dolor cupidatat adipisicing enim reprehenderit anim. Veniam elit exercitation irure proident.',
+            picture: null
+        }
+    ]
+})
